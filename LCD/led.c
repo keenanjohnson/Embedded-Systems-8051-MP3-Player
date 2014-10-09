7f7fa72b386c06086a51fed1c6b7951b139fd90e
keenanjohnson <keenan.johnson@gmail.com>
2014-10-08 23:33:36 -0500

    Delay function tested

--- LCD/led.c
+++ LCD/led.c
@@ -1,20 +1,23 @@
 #include "led.h"
 
-static unsigned uint8 code state[] = {0x04, 0x03, 0x02, 0x01};
-static unsigned uint8 num_states = 4;
+static uint8 code state[] = {0x04, 0x03, 0x02, 0x01};
+static uint8 num_states = 4;
 
 void led()
 {
 	uint32 led_delay=1000;
-	unsigned uint8 left_right=1;
+	uint8 left_right=1;
+
 	while (left_right == 1)
 	{
 		if ( i == num_states)
 			i=0;
+
 		P2=~states[i];
 		i++;
 		delay(led_delay);
 	}
+
 	while (left_right != 0)
 	{
 		if ( i < 0 )
--- LCD/main.c
+++ LCD/main.c
@@ -6,13 +6,23 @@
 // CpE 4160
 // FALL 2014
 
-#include "Main.h"
+#include "delay.h"
 #include "lcd.h"
+#include "led.h"
 #include "PORT.h"
 
 void main(void)
 {
+
+	lcd_init();
+
 	while(1)
 	{
+		REDLED = 0;
+		delay( 500 );
+		REDLED = 1;
+		delay( 500 );
+	    
+		lcd_write_data( 0x41 );
 	}
 }
