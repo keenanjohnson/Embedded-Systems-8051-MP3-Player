8bc79cc9f59aa36e22f84cd1bbc9a385f7bea177
keenanjohnson <keenan.johnson@gmail.com>
2014-10-28 12:46:12 -0500

    Fixed some project errors

--- SPI/main.c
+++ SPI/main.c
@@ -10,12 +10,18 @@
 // CpE 4160
 // FALL 2014
 
+#include "Main.h"
 #include "PORT.h"
-#include "sdcard.h"
-#include "spi.h"
+#include "uart.h"
+//#include "sdcard.h"
+#include "serial_ln.h"
+//#include "spi.h"
 
 void main(void)
 {
+	// Init UART
+	uart_init();
+
 	while(1)
 	{
         
---
+++ SPI/serial_ln.h
@@ -0,0 +1,20 @@
+#ifndef _Serial_In_H
+#define _Serial_In_H
+				 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "main.h"
+#include "UART.h"
+
+#define BS (0x08)
+#define space (0x20)
+#define DEL (0x7F)
+#define LF (0x0A)
+#define CR (0x0D)
+
+// ------ Public function prototypes -------------------------------
+
+uint32 long_serial_input(void);
+
+#endif
