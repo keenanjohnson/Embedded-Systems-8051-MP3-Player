2c88176377e3b16a8fe48b5982ff9a7c8d248e33
keenanjohnson <keenan.johnson@gmail.com>
2014-10-27 21:51:27 -0500

    Adding main file

---
+++ SPI/main.c
@@ -0,0 +1,23 @@
+// main.c
+//
+// Keenan Johnson
+// Franco Santa-Maria
+//
+// Submission for experiment 3
+//
+// SD Card
+//
+// CpE 4160
+// FALL 2014
+
+#include "PORT.h"
+#include "sdcard.h"
+#include "spi.h"
+
+void main(void)
+{
+	while(1)
+	{
+        
+	}
+}
