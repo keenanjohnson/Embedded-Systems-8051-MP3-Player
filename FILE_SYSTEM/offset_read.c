9f46dcd0d9b6eb6ee5ec9654970d26efe2ddef90
keenanjohnson <keenan.johnson@gmail.com>
2014-11-17 14:36:25 -0600

    Added offset functions

--- FILE_SYSTEM/offset_read.c
+++ FILE_SYSTEM/offset_read.c
@@ -13,21 +13,85 @@
 
 uint8 read8(uint16 offset, uint8 *array_name)
 {
+	// Declare return value
 	uint8 value = 0;
+
+	// Check that offset is < 512
+	if (offset < 512)
+	{
+		// Get value from array
+		value = *(array_name + offset);
+	}
+	else // Offset > 511
+	{
+		printf( "Offset greater than 511" );
+	}
  
 	return value; 
 }
 
 uint16 read16(uint16 offset, uint8 *array_name)
 {
+	// Declare return value
 	uint16 value = 0;
- 
+
+	// Other variable declarations
+	uint8 i;
+	uint8 byte;
+	
+	// Check that offset is < 512
+	if (offset < 512)
+	{
+		// Read both bytes of uint16
+		for( i=0; i<2; i++ )
+		{
+			// Read each byte starting with MSB
+			byte = *( array_name + offset + (1-i) );
+	
+			// Shift return value left 8 bits
+			value = value << 8;
+	
+			// OR byte with shifted return value
+			value = value | byte;
+		}
+	}
+	else // Offset > 511
+	{
+		printf( "Offset greater than 511" );
+	}
+
 	return value; 
 }
 
 uint32 read32(uint16 offset, uint8 *array_name)
 {
+	// Declare return value
 	uint32 value = 0;
+
+	// Other variable declarations
+	uint8 i;
+	uint8 byte;
+
+	// Check that offset is < 512
+	if (offset < 512)
+	{
+		// Read both bytes of uint16
+		for( i=0; i<4; i++ )
+		{
+			// Read each byte starting with MSB
+			byte = *( array_name + offset + (3-i) );
+	
+			// Shift return value left 8 bits
+			value = value << 8;
+	
+			// OR byte with shifted return value
+			value = value | byte;
+		}
+	}
+	else // Offset > 511
+	{
+		printf( "Offset greater than 511" );
+	}
  
 	return value; 
 }
--- FILE_SYSTEM/offset_read.h
+++ FILE_SYSTEM/offset_read.h
@@ -12,12 +12,29 @@
 #ifndef _OFFSET_READ_H
 #define _OFFSET_READ_H
 
+#include <stdio.h>
+
 #include "Main.h"
 
+// Each of these functions reads a specfic datatype
+// from a given 512 block of XRAM data.
+// 
+// offset - this is the offset of the data to read
+// in the XRAM block. This value must be less than 
+// 512. If this value is greater than 511, the function
+// will return 0 and print an error message to the 
+// serial port
+//
+// array_name - this is the block of data from which
+// to read
+
+// Reads a UINT8
 uint8 read8(uint16 offset, uint8 *array_name);
 
+// Reads a UINT16
 uint16 read16(uint16 offset, uint8 *array_name);
 
+// Reads a UINT32
 uint32 read32(uint16 offset, uint8 *array_name);
 
 #endif //OFFSET_READ_H
