47b2a90972427b9cd9fad2b4c8e457433a25ca67
Keenan Johnson <keenan.johnson@gmail.com>
2014-12-01 14:08:35 -0600

    Adding delay files

---
+++ I2C/delay.h
@@ -0,0 +1,23 @@
+// delay.h
+//
+// Keenan Johnson
+// Franco Santa-Maria
+//
+// CpE 4160
+// FALL 2014
+
+#ifndef _DELAY_H
+#define _DELAY_H
+
+#include "main.h"
+
+#define PRELOAD1 (65536 - (uint16)(OSC_FREQ / (OSC_PER_INST * 1020)))
+#define PRELOADH1 (PRELOAD1 / 256)
+#define PRELOADL1 (PRELOAD1 % 256)
+
+////////////////////////////////////////////////////////
+// Function to delay a specified number of miliseconds
+////////////////////////////////////////////////////////
+void delay( uint32 ms_to_delay );
+
+#endif
