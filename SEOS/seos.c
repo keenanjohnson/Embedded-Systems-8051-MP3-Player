e200c54390497b23e00f34edae6d15e69644f917
keenanjohnson <keenan.johnson@gmail.com>
2014-12-14 21:35:37 -0600

    Moved state machine to isr

--- SEOS/seos.c
+++ SEOS/seos.c
@@ -68,82 +68,7 @@ void seos_run( uint32 First_clus )
     // ADD TIMEOUT
     while(1)
 	{
-		// Basic state machine
-		switch( STATE )
-		{
-			case FIND_CLUS_1:
-				// Find Cluster
-				STATE = DATA_IDLE_2;
-				break;
-			case LOAD_BUF_1:
-				// Load Buffer
-				STATE = DATA_IDLE_2;
-				break;
-			case DATA_IDLE_1:
-				if( DATA_REQ == DR_ACTIVE )
-					STATE = DATA_SEND_1;
-				break;
-			case DATA_SEND_1:
-				if( (Buffer1Empty_g && Buffer2Empty_g) || ((DATA_REQ == DR_INACTIVE) && Buffer2Empty_g) )
-				{
-                    if( ClusterEmpty_g )
-                    {
-                        STATE = FIND_CLUS_2;
-                    }
-                    else
-                    {
-                        STATE = LOAD_BUF_2;
-                    }
-                }
-				else if( Buffer1Empty_g )
-				{
-					STATE = DATA_SEND_2;
-				}
-                if( DATA_REQ == DR_INACTIVE )
-					STATE = DATA_IDLE_1;
-				else
-				{
-					// Send Buffer 1
-				}
-				break;
-			case FIND_CLUS_2:
-				// Find Cluster
-				STATE = DATA_IDLE_1;
-				break;
-			case LOAD_BUF_2:
-				// Load Buffer
-				STATE = DATA_IDLE_1;
-				break;
-			case DATA_IDLE_2:
-				if( DATA_REQ == DR_ACTIVE )
-				{
-					STATE = DATA_SEND_2;
-				}
-				break;
-			case DATA_SEND_2:
-				if( ((DATA_REQ == DR_INACTIVE) && Buffer1Empty_g) || (Buffer1Empty_g && Buffer2Empty_g) )
-				{
-                    if( ClusterEmpty_g )
-                    {
-                        STATE = FIND_CLUS_1;
-                    }
-                    else
-                    {
-                        STATE = LOAD_BUF_1;
-                    }
-				}
-				else if( Buffer2Empty_g )
-				{
-					STATE = DATA_SEND_2;
-				}
-                if( DATA_REQ == DR_INACTIVE )
-					STATE = DATA_IDLE_2;
-				else
-				{
-					// Send Buffer 2
-				}
-				break;
-		}
+        // Do Nothing!
 	}
 }
 
@@ -152,7 +77,82 @@ void Timer2_ISR (void) interrupt Timer_2_Overflow
     // Clear flag
     TF2=0; 
 
-	GREENLED=ON;
+	// Basic state machine
+    switch( STATE )
+    {
+        case FIND_CLUS_1:
+            // Find Cluster
+            STATE = DATA_IDLE_2;
+            break;
+        case LOAD_BUF_1:
+            // Load Buffer
+            STATE = DATA_IDLE_2;
+            break;
+        case DATA_IDLE_1:
+            if( DATA_REQ == DR_ACTIVE )
+                STATE = DATA_SEND_1;
+            break;
+        case DATA_SEND_1:
+            if( (Buffer1Empty_g && Buffer2Empty_g) || ((DATA_REQ == DR_INACTIVE) && Buffer2Empty_g) )
+            {
+                if( ClusterEmpty_g )
+                {
+                    STATE = FIND_CLUS_2;
+                }
+                else
+                {
+                    STATE = LOAD_BUF_2;
+                }
+            }
+            else if( Buffer1Empty_g )
+            {
+                STATE = DATA_SEND_2;
+            }
+            if( DATA_REQ == DR_INACTIVE )
+                STATE = DATA_IDLE_1;
+            else
+            {
+                // Send Buffer 1
+            }
+            break;
+        case FIND_CLUS_2:
+            // Find Cluster
+            STATE = DATA_IDLE_1;
+            break;
+        case LOAD_BUF_2:
+            // Load Buffer
+            STATE = DATA_IDLE_1;
+            break;
+        case DATA_IDLE_2:
+            if( DATA_REQ == DR_ACTIVE )
+            {
+                STATE = DATA_SEND_2;
+            }
+            break;
+        case DATA_SEND_2:
+            if( ((DATA_REQ == DR_INACTIVE) && Buffer1Empty_g) || (Buffer1Empty_g && Buffer2Empty_g) )
+            {
+                if( ClusterEmpty_g )
+                {
+                    STATE = FIND_CLUS_1;
+                }
+                else
+                {
+                    STATE = LOAD_BUF_1;
+                }
+            }
+            else if( Buffer2Empty_g )
+            {
+                STATE = DATA_SEND_2;
+            }
+            if( DATA_REQ == DR_INACTIVE )
+                STATE = DATA_IDLE_2;
+            else
+            {
+                // Send Buffer 2
+            }
+            break;
+    }
 }
 
 void Timer2_ISR_Init( void ) 
